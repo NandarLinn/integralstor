c4a32d4c54d3bb6980537f70255b625ff8452973
NandarLinn <43093797+NandarLinn@users.noreply.github.com>
2022-01-12 20:09:39 +0630

    change current running path

--- integral_view/wsgi.py
+++ integral_view/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 import sys
-sys.path.append('/opt/integralstor/integralstor')
+sys.path.append('/opt/integralstor/integralstor2')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "integral_view.settings")
 
